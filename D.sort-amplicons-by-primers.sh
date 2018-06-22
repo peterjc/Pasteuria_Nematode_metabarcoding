826227ca0b02e8bd6f19e8188c99991f45d1a4b1
Jamie Orr <JamieNeilOrr@gmail.com>
2018-06-22 10:13:03 +0100

    Invoke primer sorting python shell script

---
+++ D.sort-amplicons-by-primers.sh
@@ -0,0 +1,5 @@
+#change into the directory which has your PEAR assembled reads #
+cd /home/jo42324/metabarcode/analysis/sequence-analysis/final-pipe/1_QC-and-assembly
+
+# invoke the primer pair sorting python script #
+python D.primer-sort-allow-mismatch-of-1nt.py
