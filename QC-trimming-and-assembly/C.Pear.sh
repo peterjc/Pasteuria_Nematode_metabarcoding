7b22415f100f1dfab886b8a95eb4f0c35a092b0b
Jamie Orr <JamieNeilOrr@gmail.com>
2018-06-26 15:42:56 +0100

    Update C.Pear.sh

--- QC-trimming-and-assembly/C.Pear.sh
+++ QC-trimming-and-assembly/C.Pear.sh
@@ -1,4 +1,4 @@
-cd /home/jo42324/metabarcode/analysis/sequence-analysis/final-pipe/1_QC-and-assembly
+cd /home/jo42324/metabarcode/analysis/sequence-analysis/Pasteuria_Nematode_metabarcoding/QC-trimming-and-assembly
 
 # Assemble trimmed PE illumina reads with PEAR 
 /home/jo42324/Download/PEAR-master/bin/pear -f trim-q22-minlen150/10623_metagen_trimmed_f.fq -r trim-q22-minlen150/10623_metagen_trimmed_r.fq -j 16 -o pear_assembled-default-q22-minlen150
