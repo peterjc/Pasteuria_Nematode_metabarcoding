38cd3ea006098ae6b1eaee6b5f568bc71eeb658e
BioJNO <JamieNeilOrr@gmail.com>
2018-10-05 10:52:52 +0100

    Belatedly check in QC scripts D and E

--- QC-trimming-and-assembly/D.primer-sort-allow-mismatch-of-1nt.py
+++ QC-trimming-and-assembly/D.primer-sort-allow-mismatch-of-1nt.py
@@ -1,21 +1,24 @@
-
-# import from Biopython FastqGeneralIterator, which is a faster way to parse fastq files #
+# Import from Biopython FastqGeneralIterator,
+# which is a faster way to parse fastq files.
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
-# import regex which allows regular expression matching with mismatch # 
+
+# Import regex which allows regular expression matching with mismatch.
 import regex
 
-
-# write an output file to store all sorted assembled reads #
+# Write an output file to store all sorted assembled reads.
 outfile1 = open("Pas-amplicons-containing-FandR-correct-orientation.fastq", "w")
-# Sometimes about half the reads are reverse oriented, so we'll look for those too #
+
+# Sometimes about half the reads are reverse oriented,
+# so we'll look for those too.
 outfile2 = open("Pas-amplicons-containing-FandR-reverse-orientation.fastq", "w")
 outfile3 = open("Nem-amplicons-containing-FandR-correct-orientation.fastq", "w")
 outfile4 = open("Nem-amplicons-containing-FandR-reverse-orientation.fastq", "w")
-outfile5 = open("Seb-amplicons-containing-fandr-correct-orientation.fastq", "w")
-outfile6 = open("Seb-amplicons-containing-fandr-reverse-orientation.fastq", "w")
+outfile5 = open("Cyt-amplicons-containing-fandr-correct-orientation.fastq", "w")
+outfile6 = open("Cyt-amplicons-containing-fandr-reverse-orientation.fastq", "w")
 outfile7 = open("no-primer-pair-found.fastq", "w")
 
-# Compile regular expressions (string) for each primer, in each orientation allowing a maximum mismatch of 1nt ({s<=1} #
+# Compile regular expressions (string) for each primer
+# in each orientation, allowing a maximum mismatch of 1nt ({s<=1}).
 
 PasF = regex.compile("(CAGCATCTTTGTGCCGAAGG){s<=1}")
 PasR_rc = regex.compile("(TTGGAGAGACAGCCGGCG){s<=1}")
@@ -29,58 +32,35 @@ NemR_rc = regex.compile("(ATTACGTCCCTGCCCTTTGTA){s<=1}")
 NemR = regex.compile("(TACAAAGGGCAGGGACGTAAT){s<=1}")
 NemF_rc = regex.compile("(AACTAAGAACGGCCATGCACCACC){s<=1}")
 
-# These are primers used in a seperate study which shared the same sequncing run #
-SebF = regex.compile("(CTTGAAGACCTTCTGTAAAAATG){s<=1}")
-SebR_rc = regex.compile("(CTCTTAAGACGGTAGCTCG){s<=1}")
+# These are primers used in a seperate study
+# which shared the same sequncing run
+CytF = regex.compile("(CTTGAAGACCTTCTGTAAAAATG){s<=1}")
+CytR_rc = regex.compile("(CTCTTAAGACGGTAGCTCG){s<=1}")
 
-SebR = regex.compile("(CGAGCTACCGTCTTAAGAG){s<=1}")
-SebF_rc = regex.compile("(CATTTTTACAGAAGGTCTTCAAG){s<=1}")
+CytR = regex.compile("(CGAGCTACCGTCTTAAGAG){s<=1}")
+CytF_rc = regex.compile("(CATTTTTACAGAAGGTCTTCAAG){s<=1}")
 
-# open and store the input file handle as a variable #
+# Open and store the input file handle as a variable.
 input_handle = open("pear_assembled-default-q22-minlen150.assembled.fastq")
 
-# for each fastq record in the assembled read file... #
+# For each fastq record in the assembled read file...
 for title, seq, qual in FastqGeneralIterator(input_handle):
-	# search for each primer pair in the assembled reads in forward... #
-	if PasF.search(seq) and PasR_rc.search(seq):
-	    outfile1.write("@%s\n%s\n+\n%s\n" % (title, seq, qual))
-	# and, if not, reverse orientation. #
-	elif PasR.search(seq) and PasF_rc.search(seq):
-		outfile2.write("@%s\n%s\n+\n%s\n" % (title, seq, qual))
-	elif NemF.search(seq) and NemR_rc.search(seq):
-		outfile3.write("@%s\n%s\n+\n%s\n" % (title, seq, qual))
-	elif NemR.search(seq) and NemF_rc.search(seq):
-		outfile4.write("@%s\n%s\n+\n%s\n" % (title, seq, qual))
-	elif SebF.search(seq) and SebR_rc.search(seq):
-		outfile5.write("@%s\n%s\n+\n%s\n" % (title, seq, qual))
-	elif SebR.search(seq) and SebF_rc.search(seq):
-		outfile6.write("@%s\n%s\n+\n%s\n" % (title, seq, qual))
-	# If no primer pairs are found in any orientation spit them out into this file so that you can check sorting is working as you want it to. #
-	else: 
-		outfile7.write("@%s\n%s\n+\n%s\n" % (title, seq, qual))
-
-
-# Close all the output files when you're done. # 
-outfile1.close()
-outfile2.close()
-outfile3.close()
-outfile4.close()
-outfile5.close()
-outfile6.close()
-outfile7.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # search for each primer pair in the assembled reads in forward...
+    if PasF.search(seq) and PasR_rc.search(seq):
+        outfile1.write("@%s\n%s\n+\n%s\n" % (title, seq, qual))
+    # and, if not, reverse orientation.
+    elif PasR.search(seq) and PasF_rc.search(seq):
+        outfile2.write("@%s\n%s\n+\n%s\n" % (title, seq, qual))
+    elif NemF.search(seq) and NemR_rc.search(seq):
+        outfile3.write("@%s\n%s\n+\n%s\n" % (title, seq, qual))
+    elif NemR.search(seq) and NemF_rc.search(seq):
+        outfile4.write("@%s\n%s\n+\n%s\n" % (title, seq, qual))
+    elif CytF.search(seq) and CytR_rc.search(seq):
+        outfile5.write("@%s\n%s\n+\n%s\n" % (title, seq, qual))
+    elif CytR.search(seq) and CytF_rc.search(seq):
+        outfile6.write("@%s\n%s\n+\n%s\n" % (title, seq, qual))
+    # If no primer pairs are found in any orientation
+    # spit them out into this file so that you can check
+    # sorting is working as you want it to.
+    else:
+        outfile7.write("@%s\n%s\n+\n%s\n" % (title, seq, qual))
--- QC-trimming-and-assembly/E.correct-orientation.py
+++ QC-trimming-and-assembly/E.correct-orientation.py
@@ -1,35 +1,34 @@
-# Now that we've sorted the amplicons into primer sets we need to flip the amplicons which assemble in the reverse orientation back into the expected orientation #
+# Now that we've sorted the amplicons into primer sets
+# we need to flip the amplicons which assemble in the
+# reverse orientation back into the expected orientation.
 
-# Import the required modules from Biopython #
-from Bio.Seq import Seq
-from Bio.Alphabet import generic_dna 
+# Import the required modules from Biopython.
+from Bio.Alphabet import generic_dna
 from Bio import SeqIO
 
-# Open output files to store the flipped amplicons #
+# Open output files to store the flipped amplicons.
 outfile1 = open("pas-amplicons-orientation-corrected.fastq", "w")
 outfile2 = open("nem-amplicons-orientation-corrected.fastq", "w")
-outfile3 = open("seb-amplicons-orientation-corrected.fastq", "w")
+outfile3 = open("cyt-amplicons-orientation-corrected.fastq", "w")
 
-# For each fastq record in the reverse orientation output from the previous step ...#
+# For each fastq record in the reverse
+# orientation output from the previous step...
 for seq_record in SeqIO.parse("Pas-amplicons-containing-FandR-reverse-orientation.fastq", "fastq"):
-	# ... flip it. Keeping the name and appending (RC) so that later on we can see it was flipped. #
-	rcseq = seq_record.reverse_complement(id=True, description=seq_record.description + " (RC)")
-	# Then write it to the output file #
-	SeqIO.write(rcseq, outfile1, "fastq")
-	
+    # ...flip it. Keeping the name and appending "(RC)"
+    # to the title so that later on we can see it was flipped.
+    rcseq = seq_record.reverse_complement(id=True, description=seq_record.description + " (RC)")
+    # Then write it to the output file.
+    SeqIO.write(rcseq, outfile1, "fastq")
 
 for seq_record in SeqIO.parse("Nem-amplicons-containing-FandR-reverse-orientation.fastq", "fastq"):
-	rcseq = seq_record.reverse_complement(id=True, description=seq_record.description + " (RC)")
-	SeqIO.write(rcseq, outfile2, "fastq")
-	
+    rcseq = seq_record.reverse_complement(id=True, description=seq_record.description + " (RC)")
+    SeqIO.write(rcseq, outfile2, "fastq")
 
+for seq_record in SeqIO.parse("Cyt-amplicons-containing-fandr-reverse-orientation.fastq", "fastq"):
+    rcseq = seq_record.reverse_complement(id=True, description=seq_record.description + " (RC)")
+    SeqIO.write(rcseq, outfile3, "fastq")
 
-for seq_record in SeqIO.parse("Seb-amplicons-containing-fandr-reverse-orientation.fastq", "fastq"):
-	rcseq = seq_record.reverse_complement(id=True, description=seq_record.description + " (RC)")
-	SeqIO.write(rcseq, outfile3, "fastq")
-	
-	
-# Close all the output files when you're done #	
+# Close all the output files when you're done.
 outfile1.close()
 outfile2.close()
 outfile3.close()
